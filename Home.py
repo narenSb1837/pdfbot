959f1d8a2e6cc312fc9326b857dae8abb57cae6f
narenSb1837 <89464601+narenSb1837@users.noreply.github.com>
2023-12-28 22:47:49 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Langchain Chatbot",
+    page_icon='💬',
+    layout='wide'
+)
+
+st.header("Chatbot Implementations with Langchain")
+
+st.write("""
+Langchain is a powerful framework designed to streamline the development of applications using Language Models (LLMs). It provides a comprehensive integration of various components, simplifying the process of assembling them to create robust applications.
+
+Leveraging the power of Langchain, the creation of chatbots becomes effortless. Here are a few examples of chatbot implementations catering to different use cases:
+
+The integration of chat functionality with PDF capabilities serves as an indispensable tool for enhanced communication and collaboration. Incorporating chat within PDF platforms offers real-time interaction, enabling users to discuss, clarify, and collaborate on document content efficiently. This feature facilitates seamless communication, allowing individuals or teams to exchange ideas, provide feedback, resolve queries, and make necessary revisions instantaneously. 
+To explore sample usage of chatbot, please navigate to the  chatbot section.
+""")
